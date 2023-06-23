cffe4e83455e70cf1710e7a79c256bafe6c757f7
Jerick Abraham <jja98@drexel.edu>
2023-06-23 11:04:12 -0400

    Creates the basic website pages and connects to the py page.

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Route for the about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route for the contact page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run()
